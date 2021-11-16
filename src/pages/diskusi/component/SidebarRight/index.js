29f1f669113b5e24c7f10f17163623bd7f8569f5
lapakbelajar <lapakbelajaridn@gmail.com>
2021-11-16 15:15:20 +0700

    mengintegrasikan komponen trending dengan api

--- src/pages/diskusi/component/SidebarRight/index.js
+++ src/pages/diskusi/component/SidebarRight/index.js
@@ -2,8 +2,37 @@ import style from "./Sidebar.module.css";
 
 // component
 import Link from "next/link";
+import api from "../../../../config/api";
+import { useEffect, useState } from "react";
 
 export default function SidebarRight() {
+  const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    getTrends();
+  }, [trending]);
+
+  /**
+   * fungsi dibawah ini digunakan untuk mengambil data trending
+   */
+
+  function getTrends() {
+    fetch(`${api.api_endpoint}/forum/stats/ambil`, {
+      headers: {
+        authorization: api.authorization,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((final) => {
+        setTrending(final);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
       <div className={style.sidebar_right}>
@@ -14,18 +43,18 @@ export default function SidebarRight() {
           </div>
 
           {/*  */}
-          <div className={style.content_box}>
-            <Link href="/">
-              <a className={style.link_box}>Ujian SBMTPN 2022</a>
-            </Link>
-            <small>22 respon</small>
-          </div>
-          <div className={style.content_box}>
-            <Link href="/">
-              <a className={style.link_box}>Try Out SBMPTN</a>
-            </Link>
-            <small>22 respon</small>
-          </div>
+          {trending.map((items, i) => (
+            <div className={style.content_box} key={i}>
+              <Link href={`/diskusi/detail/${items.identitas}`}>
+                <a className={style.link_box}>
+                  {items.pertanyaan.length > 25
+                    ? `${items.pertanyaan.slice(0, 25)}...`
+                    : items.pertanyaan}
+                </a>
+              </Link>
+              <small>{items.anonim ? "oleh anonim" : items.user.name}</small>
+            </div>
+          ))}
         </div>
 
         {/* diskusi user */}
