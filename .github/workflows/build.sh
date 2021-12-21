a9144bd09cf7eec3e9c2b081076759b9932b1121
rizki <47466861+rizki4106@users.noreply.github.com>
2021-12-21 08:58:55 +0700

    Create build.sh

---
+++ .github/workflows/build.sh
@@ -0,0 +1,12 @@
+echo "Pergi ke folder frontend"
+cd /home/dev/Web/frontend
+
+echo "Menginstall dependencies"
+rm -rf package-lock.json
+npm install
+
+echo "Build Halaman"
+npm run build
+
+echo "Restart Server"
+pm2 restart frontend
